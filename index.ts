87457b999066cc2ca6021b29f1ccb28e4c3f45c7
Idrees <dargahidrees@gmail.com>
2021-05-20 07:53:56 +0530

    feat: add compose

--- index.ts
+++ index.ts
@@ -17,9 +17,16 @@ const combineReducers = (reducers: Reducers): Reducer => {
   };
 };
 
+const compose = (...funcs: Function[]) => {
+  return function (x) {
+    return funcs.reduceRight((v, f) => f(v), x);
+  };
+};
+
 const IRedux = {
   createStore: IStore,
   combineReducers,
+  compose,
 };
 
 export { IRedux };
